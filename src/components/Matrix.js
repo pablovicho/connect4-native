3d51ce2a2723d37ad35e21edd0eadb884bce862e
Pablo.ValdesVillarreal@ab-inbev.com <Pablo.ValdesVillarreal@ab-inbev.com>
2023-06-27 15:47:38 -0600

    refactor matrix component

--- src/components/Matrix.js
+++ src/components/Matrix.js
@@ -20,32 +20,21 @@ const Matrix = () => {
 
   return (
     <div className="matrix">
-      {winner === 0
-        ? matrix
-            .map((row, index) => {
-              return row.map((element, i) => {
-                return (
-                  <div key={`${index},${i}`} onClick={(e) => HandleClick(e, i)}>
-                    <Circle player={element} />
-                  </div>
-                );
-              });
-            })
-            .reverse()
-        
-            : 
-        
-        matrix
-            .map((row, index) => {
-              return row.map((element, i) => {
-                return (
-                  <div key={`${index},${i}`}>
-                    <Circle player={element} />
-                  </div>
-                );
-              });
-            })
-            .reverse()}
+      {matrix
+        .map((row, index) => {
+          return row.map((element, i) => {
+            return winner === 0 ? (
+              <div key={`${index},${i}`} onClick={(e) => HandleClick(e, i)}>
+                <Circle player={element} />
+              </div>
+            ) : (
+              <div key={`${index},${i}`}>
+                <Circle player={element} />
+              </div>
+            );
+          });
+        })
+        .reverse()}
     </div>
   );
 };
