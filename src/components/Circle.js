1e436264c850f43c64fc1423fe5e8ffa8d60530a
Pablo.ValdesVillarreal@ab-inbev.com <Pablo.ValdesVillarreal@ab-inbev.com>
2023-07-15 09:36:58 -0600

    delete animation at win

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./../App";
+import useStore from "../utils/store";
 
 export default function Circle(props) {
+  const winner = useStore((state) => state.winner);
   const player = props.player;
   const colorClass = ["white", "yellow", "red"];
 
-  return (
+  return winner === 0 ? (
       <div className= {`circle ${colorClass[player]}`}></div>
+  )
+  :
+  (
+    <div className= {`circle ${colorClass[player]} no-animate` }></div>
   );
 }
